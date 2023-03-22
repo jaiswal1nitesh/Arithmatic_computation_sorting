dea02f444efb770b5570f20cfa8d862b7763ff25
jaiswal1nitesh <nj639476@gmail.comgit config --global user.email nj639476@gmail.com>
2023-03-22 19:35:43 +0530

    Add : Take three input from the user

---
+++ arith_sort.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+echo "Enter the three Number"
+echo
+read -p "Enter the First number  ---> " num1
+read -p "Enter the Second number ---> " num2
+read -p "Enter the third number  ---> " num3
+
