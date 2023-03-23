eced26b65c5f0effe4514feaa1645d903759a997
jaiswal1nitesh <nj639476@gmail.comgit config --global user.email nj639476@gmail.com>
2023-03-23 13:25:55 +0530

    [Nitseh] Refactor change name of variable and store in array

--- arith_sort.sh
+++ arith_sort.sh
@@ -4,19 +4,20 @@ echo
 read -p "Enter the First number  ---> " a
 read -p "Enter the Second number ---> " b
 read -p "Enter the third number  ---> " c
-let x=a+b*c
-let y=a*b+c
-let z=c+a/b
-let p=a%b+c
+let uc1=a+b*c
+let uc2=a*b+c
+let uc3=c+a/b
+let uc4=a%b+c
+echo " uc1=$uc1  uc2=$uc2  uc3=$uc3  uc4=$uc4"
 declare -A dict
-dict[1]=$x
-dict[2]=$y
-dict[3]=$z
-dict[4]=$p
-echo ${dict[@]}
-echo ${!dict[@]}
+dict[1]=$uc1
+dict[2]=$uc2
+dict[3]=$uc3
+dict[4]=$uc4
+echo " Computation store in dict ${dict[@]} "
+#echo ${!dict[@]}
 for ((j=1,i=0; i<${#dict[@]}; i++,j++))
 do
 	arr[$i]=${dict[$j]}
 done
-echo ${arr[@]}
+echo " Computation store in array ${arr[@]} "
